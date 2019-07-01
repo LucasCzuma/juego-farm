6a5ef63205f68663f4897922ef3a6007dcf7334e
Lucas <lucasczumadewski@gmail.com>
2019-07-01 01:41:43 -0300

    Agregue caras de los muchachos para modificar el juego en caso de quererlo

--- js.js
+++ js.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 var canvas_fondo = document.getElementById('canvas_fondo');
 var canvas_context = canvas_fondo.getContext('2d');
 
